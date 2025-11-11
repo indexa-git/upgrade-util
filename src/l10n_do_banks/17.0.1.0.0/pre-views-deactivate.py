93a4b11de8fb16981122bed270f58e10269a6c7e
gerald-pcg <gerald@progressa.group>
2025-11-11 15:31:58 -0400

    imp(l10n_do_banks): pre-views_deactivate.py script added

---
+++ src/l10n_do_banks/17.0.1.0.0/pre-views-deactivate.py
@@ -0,0 +1,33 @@
+from odoo import api, SUPERUSER_ID
+import logging
+
+_logger = logging.getLogger(__name__)
+
+def migrate(cr, version):
+    """
+    Deactivates specific Odoo views.
+    
+    Args:
+        cr (cursor): Database cursor
+        version (str): Module version
+    """
+    _logger.info('Deactivating specific views')
+    env = api.Environment(cr, SUPERUSER_ID, {})
+
+    views_to_deactivate = [
+        'portal.frontend_layout',
+    ]
+
+    for xml_id in views_to_deactivate:
+        try:
+            view = env.ref(xml_id)
+            if view:
+                view.write({'active': False})
+                _logger.info(f'View deactivated: {xml_id}')
+            else:
+                _logger.warning(f'View not found: {xml_id}')
+        except Exception as e:
+            _logger.warning(f'Error deactivating view {xml_id}: {e}')
+
+    _logger.info('Finished deactivating views')
+
